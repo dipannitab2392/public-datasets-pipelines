72c1746d33d71a2af821eb0240311bd9f99beaa5
Dipannita Banerjee <dipannitab@google.com>
2021-08-25 10:47:16 +0000

    feat: Onboard IRS 990 Dataset

--- datasets/irs_990/irs_990_pf_2016/irs_990_pf_2016_dag.py
+++ datasets/irs_990/irs_990_pf_2016/irs_990_pf_2016_dag.py
@@ -14,7 +14,9 @@
 
 
 from airflow import DAG
-from airflow.contrib.operators import gcs_to_bq, kubernetes_pod_operator
+from airflow.contrib.operators import gcs_to_bq
+from airflow.contrib.operators import kubernetes_pod_operator
+
 
 default_args = {
     "owner": "Google",
